7c1c390887f903cfeaf249a0ad270e0e403fa615
TeamAbella060420 <jabella@rscm.com.sa>
2022-05-01 15:28:28 +0300

    Update widget.js

--- js/widget.js
+++ js/widget.js
@@ -2,7 +2,7 @@ var xhr = new XMLHttpRequest();
 xhr.onreadystatechange = function () {
     if(xhr.readyState === 4) {
        var employees = JSON.parse(xhr.responseText);
-        
+        var statusHTML = '<ul class="bulleted">';
     }
 };
 xhr.open('GET', 'data/employees.json');
